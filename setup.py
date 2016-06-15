a3e35d60ecfef4d9fad997ed266cbc8cf0f07d0c
Domen Racki <domen.racki@sensum.eu>
2016-06-15 14:35:34 +0200

    deleted files and moved to package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Distutils',
+    version='1.0',
+    description='A collection of general purpose functions for 2D/3D point manipulation',
+    author='Domen Racki',
+    url='https://github.com/domenracki/point-manipulation/tree/master/point_manipulation/',
+    packages=['numpy', 'warnings'],
+    )
